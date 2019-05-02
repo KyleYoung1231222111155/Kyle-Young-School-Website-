60c6c3bffe0b8285592f9fd8e087834be5e8a149
KyleYoung1231222111155 <48301196+KyleYoung1231222111155@users.noreply.github.com>
2019-05-02 12:40:19 +1200

    v10

---
+++ 10 26-03-2019 v2/java.js
@@ -0,0 +1,7 @@
+console.log("hello")
+
+function mymath(){
+	console.log("Math Is running")
+	var mymath = ["2+2","9+2","5+3","8+3","5+1"];//,["4","11","7","11","6"];
+	document.getElementById("askmath").innerHTML = mymath[0];
+}
