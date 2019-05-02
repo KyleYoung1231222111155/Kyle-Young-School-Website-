22a0466d19b2207c1b491f7c0063de2824314e8f
KyleYoung1231222111155 <48301196+KyleYoung1231222111155@users.noreply.github.com>
2019-05-02 12:43:10 +1200

    v13

---
+++ 13 9-04-2019/java.js
@@ -0,0 +1,23 @@
+console.log("hello")
+
+Question = function(input1,input2){
+	this.qtext = input1;
+	var answer = input2;
+	this.congrats = "Well done! It is " + answer + "!";
+	this.check = function(givenAnswer){
+		if(givenAnswer == answer){
+			alert(this.congrats)
+		}
+	}
+}
+
+var qList = [] 
+qList[0] = new Question("What is the capital of France?","paris");
+qList[1] = new Question("2+2=","4");
+
+
+function mymath(){
+	document.getElementById("askmath").innerHTML = qList[0].qtext
+	document.getElementById("qmath").onclick = "" 
+}
+//qList[0].check(prompt(qList[0].qtext));
