962e5d2431733f09fc704c8ee874e63b1d623c85
KyleYoung1231222111155 <48301196+KyleYoung1231222111155@users.noreply.github.com>
2019-05-03 10:34:24 +1200

    V16

--- Assment/java.js
+++ Assment/java.js
@@ -5,24 +5,39 @@ Question = function(input1,input2){
 	this.qtext = input1;
 	var answer = input2;
 	this.congrats = "Well done! It is " + answer + "!";
+	console.log("runningask")
 	this.check = function(givenAnswer){
 		if(givenAnswer == answer){
-			alert(this.congrats)
+			alert(this.congrats);
+			console.log("right answer running")
+		} else{
+			alert("wrong")
 		}
+		
 	}
 }
 
 var qList = [] 
-qList[0] = new Question("What is the capital of France?","paris");               //this is the Question bank
-qList[1] = new Question("2+2=","4");
+//qList[0] = new Question("2+2=","4");               //this is the Question bank
+//console.log("qlist2running");
+//qList[1] = new Question();
 
-
-function mymath(){
-	document.getElementById("askmath").innerHTML = qList[0].qtext
-	document.getElementById("textbox").value;
-	document.getElementById("qmath").onclick = "" 
+var i = 0;
+while(i<10){
+	qList[i] = new Question("2+" + i + "=",2+i);
+	console.log(qList[i])
+	i++;
 }
-//qList[0].check(prompt(qList[0].qtext));
-	
-	 
-	//alert(x)
+var j = 0
+function mark() {
+	console.log("mark running");
+	console.log(document.getElementById("textbox").value);
+	qList[j].check(document.getElementById("textbox").value);
+	j++;
+	mymath(j);
+}
+
+function mymath(n){
+	console.log("mymathrunning")
+	document.getElementById("askmath").innerHTML = qList[n].qtext
+}
