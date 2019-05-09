42e7e9d8583c770a7f76f1fcd5696692fdaf0c83
KyleYoung1231222111155 <48301196+KyleYoung1231222111155@users.noreply.github.com>
2019-05-10 10:49:38 +1200

    V19

--- Assment/java.js
+++ Assment/java.js
@@ -26,6 +26,11 @@ while(i<10){
 	qList[0] = new Question("Round The Folling Answers to the Stated number of Decimal places 4.763(1dp)","5");               //this is the Question bank
 	console.log("qlist2running");
 	qList[1] = new Question("When a grizzly bear hibernates, its heart rate drops to 10 beats per minute, which is 20%, percent of its normal value.    What is a grizzly bear's normal heart rate when not hibernating?","50");
+	qList[2] = new Question("6 box can hold 36 books what is the ratio book per box","36:6"); 
+	qList[3] = new Question("6 box can hold 36 books what is the rate book per box","6"); 
+	qList[4] = new Question("What is 25% of 908?","227"); 
+	qList[5] = new Question("15+(52)=","-37"); 
+	qList[6] = new Question("77-(62)=","139"); 
 	i++;
 }
 var j = 0
