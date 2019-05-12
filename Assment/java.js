5ff0290ce2f62bf51e5aa2de561c848e04d966f0
KyleYoung1231222111155 <48301196+KyleYoung1231222111155@users.noreply.github.com>
2019-05-13 09:48:44 +1200

    working on hint button

--- Assment/java.js
+++ Assment/java.js
@@ -1,10 +1,19 @@
+//src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+
 console.log("hello")
+
+	//$(document).ready(function(){
+	//	$(".button").click(function(){
+	//	$("h1 , h2 , p").toggleClass("black");
+		
+	//	});
+	//});
 
 Question = function(input1,input2){
 
 	this.qtext = input1;
 	var answer = input2;
-	this.congrats = "Well done! It is " + answer + "!";
+	this.congrats = "Well done! It is " + answer + "!";                        //this is how it replays to how if you get it right or wrong
 	console.log("runningask")
 	this.check = function(givenAnswer){
 		if(givenAnswer == answer){
@@ -23,9 +32,9 @@ var i = 0;
 while(i<10){
 	//qList[i] = new Question("2+" + i + "=",2+i);
 	//console.log(qList[i])
-	qList[0] = new Question("Round The Folling Answers to the Stated number of Decimal places 4.763(1dp)","5");               //this is the Question bank
+	qList[0] = new Question("Round The Folling Answers to the Stated number of Decimal places 4.763(1dp)","5","round up");               //this is the Question bank
 	console.log("qlist2running");
-	qList[1] = new Question("When a grizzly bear hibernates, its heart rate drops to 10 beats per minute, which is 20%, percent of its normal value.    What is a grizzly bear's normal heart rate when not hibernating?","50");
+	qList[1] = new Question("When a grizzly bear hibernates, its heart rate drops to 10 beats per minute, which is 20%, percent of its normal value.    What is a grizzly bear's normal heart rate when not hibernating?","50","there 60 beats per minute");
 	qList[2] = new Question("6 box can hold 36 books what is the ratio book per box","36:6"); 
 	qList[3] = new Question("6 box can hold 36 books what is the rate book per box","6"); 
 	qList[4] = new Question("What is 25% of 908?","227"); 
@@ -36,7 +45,7 @@ while(i<10){
 var j = 0
 function mark() {
 	console.log("mark running");
-	console.log(document.getElementById("textbox").value);
+	console.log(document.getElementById("textbox").value);									//this checks the value of the text and reads it and looks at the answer and see if it is right 
 	qList[j].check(document.getElementById("textbox").value);
 	j++;
 	mymath(j);
@@ -44,8 +53,13 @@ function mark() {
 
 function mymath(n){
 	console.log("mymathrunning")
-	document.getElementById("askmath").innerHTML = qList[n].qtext
+	document.getElementById("askmath").innerHTML = qList[n].qtext							//this look and reads the fist item in the array list in the question bank and puts it in the html
 	if (n==0){
 		document.getElementById("askmath").onclick = "";
 	}
+}
+
+function hint(input3){
+	console.log("hint running");		
+	document.getElementById("hint").innerHTML = qList[n].qtext
 }
