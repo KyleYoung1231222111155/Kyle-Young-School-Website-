233e1f120f2d373c52d9fe18601872667fd0f3ef
KyleYoung1231222111155 <48301196+KyleYoung1231222111155@users.noreply.github.com>
2019-06-18 15:16:28 +1200

    Add files via upload

--- java.js
+++ java.js
@@ -1,23 +1,88 @@
-console.log("hello")
+//src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
 
-Question = function(input1,input2){
-	this.qtext = input1;
-	var answer = input2;
-	this.congrats = "Well done! It is " + answer + "!";
-	this.check = function(givenAnswer){
-		if(givenAnswer == answer){
-			alert(this.congrats)
-		}
+
+
+var ras = 0													//ras = right answers, this Variables will go up every time you get a answer is right
+
+var was = 0													//was = wrong answers, this Variables will go up every time you get a answer is wrong
+
+
+
+console.log("This Is Running")
+
+	//$(document).ready(function(){
+	//	$(".button").click(function(){
+	//	$("h1 , h2 , p").toggleClass("black");
+		
+	//	});
+	//});
+
+	
+
+function mymath(){
+	var pickquestion = Math.floor((Math.random() * 3));
+	var randomnumber1 = Math.floor((Math.random() * 100) + 1);
+	var randomnumber2 = Math.floor((Math.random() * 100) + 1);
+	var randomlist = Math.floor((Math.random() * 2));
+	
+	console.log(pickquestion)
+	console.log(randomlist)
+	document.getElementById("next").innerHTML = "Next Math Questions";     		
+	console.log(randomnumber1)
+	console.log(randomnumber2)
+	if(pickquestion == 0){
+		
+		var answer = randomnumber1 + randomnumber2;
+		console.log(answer);
+	var qlist = {part1:"Whats this ", part2:" + ", part3:""}
+		document.getElementById("askmath").innerHTML = qlist.part1 + randomnumber1 + qlist.part2 + randomnumber2 + qlist.part3;    //this
+		
+	} else if (pickquestion == 1) {
+		
+		var answer = randomnumber1 * randomnumber2;
+		console.log(answer);
+	var qlist = {part1:"Whats this ", part2:" x ", part3:""}
+		document.getElementById("askmath").innerHTML = qlist.part1 + randomnumber1 + qlist.part2 + randomnumber2 + qlist.part3;    //this
+		
+	} else if (pickquestion == 2){
+		var answer = randomnumber1 - randomnumber2;
+		console.log(answer);
+	var qlist = {part1:"Whats this ", part2:" - ", part3:""}
+		document.getElementById("askmath").innerHTML = qlist.part1 + randomnumber1 + qlist.part2 + randomnumber2 + qlist.part3;    //this
+		document.getElementById("answertext").innerHTML = answer;
 	}
 }
 
-var qList = [] 
-qList[0] = new Question("What is the capital of France?","paris");
-qList[1] = new Question("2+2=","4");
 
 
-function mymath(){
-	document.getElementById("askmath").innerHTML = qList[0].qtext
-	document.getElementById("qmath").onclick = "" 
+function mark(){
+	console.log("work");
+	var getanswer =	document.getElementById("answer").value;
+		console.log(document.getElementById("answer").value);
+	var str =	document.getElementById("textbox").value;
+		console.log(document.getElementById("textbox").value);	
+	str = str.trim();	
+		console.log(str);														//this checks the value of the text and reads it and looks at the answer and see if it is right 
+		console.log("mark running");
+		console.log(str);
+	str = str.toLowerCase();
+		console.log(str);
+		console.log(getanswer);
+			if(str == getanswer){
+				ras++;
+				console.log("How Many Right Answers " + ras);
+				alert(this.congrats);
+				console.log("right answer running");
+				document.getElementById("rightanswers").innerHTML = "Right Answers " + ras;
+			} else{
+				alert("wrong")
+				was++;
+				console.log("How Many Wrong Answers " + was);
+				document.getElementById("wronganswers").innerHTML = "Wrong Answers " + was;
+		}
+
 }
-//qList[0].check(prompt(qList[0].qtext));
+
+
+
+		
