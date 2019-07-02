6d6e1bd43905ff5cc3cc6c8af38500fc47890d86
KyleYoung1231222111155 <48301196+KyleYoung1231222111155@users.noreply.github.com>
2019-07-02 15:19:29 +1200

    Add files via upload

--- 27/java.js
+++ 27/java.js
@@ -1,10 +1,5 @@
 //src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
 
-
-
-var j = 0
-
-
 console.log("hello")
 
 	//$(document).ready(function(){
@@ -13,78 +8,69 @@ console.log("hello")
 		
 	//	});
 	//});
-	
 
+Question = function(input1,input2){
 
-Question = function(){
-	console.log("making question is runing")
-	var makequestion = {questiontype: Math.floor((Math.random() * 3)), randomnumber1: Math.floor((Math.random() * 100) + 1), randomnumber2: Math.floor((Math.random() * 100) + 1)};
-	this.textboxtrim = function(){
-				var str =	document.getElementById("textbox").value;
-				console.log(document.getElementById("textbox").value);	
-				str = str.trim();	
-				console.log(str);														//this checks the value of the text and reads it and looks at the answer and see if it is right 
-				console.log("mark running");
-				console.log(str)
-				str = str.toLowerCase();
-				console.log(str);
-	}
-	this.getrightanswer = function(){
-			if (makequestion.pickquestion == 0) {	
-				var answer = makequestion.randomnumber1 + makequestion.randomnumber2;
-				console.log(answer)
-						} else if (makequestion.pickquestion == 1) {	
-							var answer = makequestion.randomnumber1 * makequestion.randomnumber2;
-							console.log(answer)
-								} else if (makequestion.pickquestion == 2){
-									var answer = makequestion.randomnumber1 - makequestion.randomnumber2;
-										console.log(answer)
-			}		
-		} 
-	this.cheak = function(){
-		var ras = 0													//ras = right answers, this Variables will go up every time you get a answer is right
-		var was = 0													//was = wrong answers, this Variables will go up every time you get a answer is wrong
-		if(str == answer){
-			console.log("good work")
-				ras++
-				document.getElementById("rightanswers").innerHTML = "Right Answer" + ras 
-				alert("Good Work")
-					}else{
-						was++
-						alert("wrong")
-						document.getElementById("wronganswers").innerHTML = "Wrong Answer" + was
-		}
-	this.questiontext = function(){
-			if(makequestion.pickquestion == 0){
-				var qlist = {part1:"Whats this ", part2:" + ", part3:""}
-					document.getElementById("askmath").innerHTML = qlist.part1 + randomnumber1 + qlist.part2 + randomnumber2 + qlist.part3;
-		
-				} else if (makequestion.pickquestion == 1) {	
-						var qlist = {part1:"Whats this ", part2:" x ", part3:""}
-						document.getElementById("askmath").innerHTML = qlist.part1 + randomnumber1 + qlist.part2 + randomnumber2 + qlist.part3;
-	
-						} else if (makequestion.pickquestion == 2){
-								var qlist = {part1:"Whats this ", part2:" - ", part3:""};
-								document.getElementById("askmath").innerHTML = qlist.part1 + randomnumber1 + qlist.part2 + randomnumber2 + qlist.part3;
-			}
-	
+	this.qtext = input1;
+	var answer = input2;
+	this.congrats = "Well done! It is " + answer + "!";                        //this is how it replays to how if you get it right or wrong
+	console.log("runningask")
+	console.log(answer)
+	this.check = function(givenAnswer){
+		console.log("Check is runing")
+		console.log(givenAnswer)
+		console.log(answer)
+		if(givenAnswer == answer){
+			alert(this.congrats);
+			console.log("right answer running")
+		} else{
+			alert("wrong")
 		}
 	}
-	
-	
-
-	function getquestion(){
-	new Question.questiontext
-	console.log(pickquestion)
-	//console.log(randimlist)
-	document.getElementById("next").innerHTML = "Next Math Questions";
-	console.log(makequstion.randomnumber1);
-	console.log(makequstion.randomnumber2);
 }
 
+var qList = [] 
+var makequestion = {questiontype: Math.floor((Math.random() * 3)), randomnumber1: Math.floor((Math.random() * 100) + 90), randomnumber2: Math.floor((Math.random() * 90) + 1)};
+var i = 0;
 
 
-	function mark(){
-		Question.getrightanswer.textboxtrim.cheak
+
+while(i<10){
+	var type = {questionpart1: "If there is ", questionpart2: " on a plane ", questionpart3: " how many got off."}
+	//qList[i] = new Question("2+" + i + "=",2+i);
+	//console.log(qList[i])
+	qList[0] = new Question(type.questionpart1 + makequestion.randomnumber1 + type.questionpart2 + makequestion.randomnumber2 + type.questionpart3, makequestion.randomnumber1-makequestion.randomnumber2,"round up");               //this is the Question bank
+	console.log("qlist2running");
+	qList[1] = new Question("When a grizzly bear hibernates, its heart rate drops to 10 beats per minute, which is 20%, percent of its normal value.    What is a grizzly bear's normal heart rate when not hibernating?","50","there 60 beats per minute");
+	qList[2] = new Question("6 box can hold 36 books what is the ratio book per box","36:6"); 
+	qList[3] = new Question("6 box can hold 36 books what is the rate book per box","6"); 
+	qList[4] = new Question("What is 25% of 908?","227"); 
+	qList[5] = new Question("15+(52)=","-37"); 
+	qList[6] = new Question("77-(62)=","139"); 
+	qList[7] = new Question("Work it Out √36","6");
+	qList[8] = new Question("Work It out √225","15");
+	qList[9] = new Question("The number of people on a plane was 280. If 40% of the people got off. How many were left?","112");
+	qList[10] = new Question("√225",15);
+	qList[11] = new Question("√225",15);
+	i++;
+}
+var j = 0
+function mark() {
+	console.log("mark running");
+	console.log(document.getElementById("textbox").value);									//this checks the value of the text and reads it and looks at the answer and see if it is right 
+	qList[j].check(document.getElementById("textbox").value);
+	j++;
+	getquestion(j);
+}
+
+function getquestion(n){
+	console.log("mymathrunning")
+	document.getElementById("askmath").innerHTML = qList[n].qtext							//this look and reads the fist item in the array list in the question bank and puts it in the html
+	if (n==0){
+		document.getElementById("askmath").onclick = "";
 	}
+}
 
+function givehint(){
+	
+}
